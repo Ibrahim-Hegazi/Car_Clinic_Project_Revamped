123747d6a1d8b0f26bf8275066774c2d6b7b5223
Ibrahim-Hegazi <ihegaziwork@gmail.com>
2025-07-28 01:12:01 +0300

    Updated the scraper imports

--- python_scripts/reddit_data_extractor/flow.py
+++ python_scripts/reddit_data_extractor/flow.py
@@ -4,7 +4,7 @@ from prefect import flow, task, get_run_logger
 from prefect.tasks import task_input_hash
 from datetime import timedelta
 
-from scraper import extract_reddit_data
+from .scraper import extract_reddit_data
 
 import sys
 from pathlib import Path
