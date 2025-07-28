1bb69a3147cbb5ebb4a457d5b5ff5cb71acacf7b
Ibrahim-Hegazi <ihegaziwork@gmail.com>
2025-07-28 22:22:22 +0300

    Moved the run_pipeline.py outside of the python script directory

--- run_pipeline.py
+++ run_pipeline.py
@@ -1,6 +1,6 @@
 # run_pipeline.py – Launch script for GitHub Actions or manual run
 
-from .flow import reddit_pipeline
+from python_scripts.reddit_data_extractor.flow import reddit_pipeline
 
 if __name__ == "__main__":
     reddit_pipeline()
